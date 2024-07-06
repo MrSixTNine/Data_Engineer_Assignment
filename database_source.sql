b2311bb4e4b3c7b96ecc42ab6bcf824db71c229f
tanatpun <tanat.punyasarn@gmail.com>
2024-07-07 03:22:56 +0700

    updated SQL

--- database_source.sql
+++ database_source.sql
@@ -12,7 +12,7 @@ CREATE TABLE pre_prd.product_catalog (
     created_at     TIMESTAMP DEFAULT timezone('Asia/Bangkok', NOW()),
     updated_by     VARCHAR(255) DEFAULT 'Admin' NOT NULL,
     updated_at     TIMESTAMP DEFAULT timezone('Asia/Bangkok', NOW()),
-    
+    CONSTRAINT product_catalog_id_pkey PRIMARY KEY (id),
     CONSTRAINT product_catalog_product_id_uniquekey UNIQUE (product_id)
 );
 
@@ -27,6 +27,24 @@ CREATE TABLE pre_prd.customer_transactions (
     created_at     TIMESTAMP DEFAULT timezone('Asia/Bangkok', NOW()),
     updated_by     VARCHAR(255) DEFAULT 'Admin' NOT NULL,
     updated_at     TIMESTAMP DEFAULT timezone('Asia/Bangkok', NOW()),
-    
+    CONSTRAINT customer_transactions_transaction_id_pkey PRIMARY KEY (transaction_id),
     CONSTRAINT customer_transaction_product_id_fkey FOREIGN KEY (product_id) REFERENCES pre_prd.product_catalog(product_id)
+);
+
+CREATE TABLE prd.product_sales_detail (
+    transaction_id	UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+    customer_id	    VARCHAR(10) NOT NULL,
+    product_id	    VARCHAR(10) NOT NULL,
+    product_name	VARCHAR(255) NOT NULL,
+    category	    VARCHAR(255) NOT NULL,
+    quantity	    INT NOT NULL,
+    price	        FLOAT NOT NULL,
+    total_price	    FLOAT NOT NULL,
+    transaction_date DATE NOT NULL;
+    created_by     VARCHAR(255) DEFAULT 'Admin' NOT NULL,
+    created_at     TIMESTAMP DEFAULT timezone('Asia/Bangkok', NOW()),
+    updated_by     VARCHAR(255) DEFAULT 'Admin' NOT NULL,
+    updated_at     TIMESTAMP DEFAULT timezone('Asia/Bangkok', NOW()),
+    CONSTRAINT product_sales_detail_transaction_id_pkey PRIMARY KEY (transaction_id),
+    CONSTRAINT product_sales_detail_product_id_fkey FOREIGN KEY (product_id) REFERENCES pre_prd.product_catalog(product_id)
 );
