03db1b26803e4a71bdea8a4bedbcffa84ec5f1d3
tanatpun <tanat.punyasarn@gmail.com>
2024-07-07 04:38:09 +0700

    add file

--- database_source.sql
+++ database_source.sql
@@ -3,7 +3,7 @@ CREATE SCHEMA pre_prd;
 CREATE SCHEMA prd;
 
 CREATE TABLE pre_prd.product_catalog (
-    id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+    id UUID DEFAULT gen_random_uuid(),
     product_id     VARCHAR(10) NOT NULL,
     product_name   VARCHAR(255) NOT NULL,	
     category       VARCHAR(255) NOT NULL,
@@ -12,12 +12,12 @@ CREATE TABLE pre_prd.product_catalog (
     created_at     TIMESTAMP DEFAULT timezone('Asia/Bangkok', NOW()),
     updated_by     VARCHAR(255) DEFAULT 'Admin' NOT NULL,
     updated_at     TIMESTAMP DEFAULT timezone('Asia/Bangkok', NOW()),
-
+    CONSTRAINT product_catalog_id_pkey PRIMARY KEY(id),
     CONSTRAINT product_catalog_product_id_uniquekey UNIQUE (product_id)
 );
 
 CREATE TABLE pre_prd.customer_transactions (
-    transaction_id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+    transaction_id UUID DEFAULT gen_random_uuid(),
     customer_id    VARCHAR(10) NOT NULL,
     product_id     VARCHAR(10) NOT NULL,
     quantity       INT NOT NULL,
@@ -27,12 +27,12 @@ CREATE TABLE pre_prd.customer_transactions (
     created_at     TIMESTAMP DEFAULT timezone('Asia/Bangkok', NOW()),
     updated_by     VARCHAR(255) DEFAULT 'Admin' NOT NULL,
     updated_at     TIMESTAMP DEFAULT timezone('Asia/Bangkok', NOW()),
-
+    CONSTRAINT customer_transactions_transaction_id_pkey PRIMARY KEY(transaction_id),
     CONSTRAINT customer_transaction_product_id_fkey FOREIGN KEY (product_id) REFERENCES pre_prd.product_catalog(product_id)
 );
 
 CREATE TABLE prd.product_sales_detail (
-    transaction_id	UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+    transaction_id	UUID DEFAULT gen_random_uuid(),
     customer_id	    VARCHAR(10) NOT NULL,
     product_id	    VARCHAR(10) NOT NULL,
     product_name	VARCHAR(255) NOT NULL,
@@ -45,6 +45,6 @@ CREATE TABLE prd.product_sales_detail (
     created_at     TIMESTAMP DEFAULT timezone('Asia/Bangkok', NOW()),
     updated_by     VARCHAR(255) DEFAULT 'Admin' NOT NULL,
     updated_at     TIMESTAMP DEFAULT timezone('Asia/Bangkok', NOW()),
-
+    CONSTRAINT product_sales_detail_transaction_id_pkey PRIMARY KEY(transaction_id),
     CONSTRAINT product_sales_detail_product_id_fkey FOREIGN KEY (product_id) REFERENCES pre_prd.product_catalog(product_id)
 );
