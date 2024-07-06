056865183806d71b2306fdcd6eb2cfc7dcf5ec3d
Tanat111 <tanat.punyasarn@mail.kmutt.ac.th>
2024-07-06 21:33:00 +0700

    bat command

---
+++ docker_command.bat
@@ -0,0 +1,19 @@
+@echo off
+
+REM Pull Docker images
+docker pull postgres:alpine
+docker pull dbgate/dbgate:alpine
+docker pull busybox
+
+REM Create Docker volumes
+docker volume create dbgate_data
+
+REM Import Volumes of Postgresql
+docker run --rm `
+  -v ${PWD}/data_engineer_assignment_postgresql_data.tar.gz:/backup/data_engineer_assignment_postgresql_data.tar.gz `
+  -v data_engineer_assignment_postgresql_data:/data `
+  busybox `
+  sh -c "tar xvf /backup/data_engineer_assignment_postgresql_data.tar.gz -C /data"
+
+REM Run Docker containers
+docker-compose up
