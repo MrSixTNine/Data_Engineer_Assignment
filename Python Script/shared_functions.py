d7b56ac3c43c79ee169ab6575c6b067dd7a2625c
Tanat111 <tanat.punyasarn@mail.kmutt.ac.th>
2024-07-07 01:07:47 +0700

    t

--- Python Script/shared_functions.py
+++ Python Script/shared_functions.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine
 def insert_into_postgres(df, table_name, schema_name, engine):
     try:
         # Insert the DataFrame into the specified table within the specified schema
-        df.to_sql(name=table_name, con=engine, schema=schema_name, if_exists='append', index=False)
+        df.to_sql(table_name, engine, if_exists='append', index=False, schema=schema_name)
         print(f"Data successfully inserted into {schema_name}.{table_name}")
     except Exception as e:
         print(f"Error: {e}")
