b40de739962b7a744391a24b6706a3b9b50d306f
Tanat111 <tanat.punyasarn@mail.kmutt.ac.th>
2024-07-07 16:41:18 +0700

    .

--- docker_command_kit.bat
+++ docker_command_kit.bat
@@ -7,6 +7,13 @@ docker pull dbgate/dbgate:alpine
 REM Create Docker volumes
 docker volume create dbgate_data
 
+REM Import Volumes of DBGate
+docker run --rm ^
+  -v %cd%/dbgate_data.tar.gz:/backup/dbgate_data.tar.gz^
+  -v dbgate_data:/data ^
+  busybox ^
+  sh -c "tar xvf /backup/dbgate_data.tar.gz -C /data"
+
 REM Build the Docker image
 docker build -t my-python-app .
 
