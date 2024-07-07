ce700cc2c75b22b903454d8020dc7333b22b7cc6
Tanat111 <tanat.punyasarn@mail.kmutt.ac.th>
2024-07-07 17:16:54 +0700

    Update docker_command_kit.bat

--- docker_command_kit.bat
+++ docker_command_kit.bat
@@ -8,10 +8,10 @@ REM Create Docker volumes
 docker volume create dbgate_data
 
 REM Import Volumes of DBGate
-docker run --rm `
-  -v %cd%\dbgate_data.tar.gz:/backup/dbgate_data.tar.gz `
-  -v dbgate_data:/data `
-  busybox `
+docker run --rm ^
+  -v %cd%/dbgate_data.tar.gz:/backup/dbgate_data.tar.gz^
+  -v dbgate_data:/data ^
+  busybox ^
   sh -c "tar xvf /backup/dbgate_data.tar.gz -C /data"
 
 REM Build the Docker image
